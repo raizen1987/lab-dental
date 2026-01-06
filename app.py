8bb8b04e1f05385fd3d7e47b48f3c1b83cc23334
raizen1987 <53264750+raizen1987@users.noreply.github.com>
2026-01-06 12:40:54 -0300

    Update app.py
    Fix puerto para Render

--- app.py
+++ app.py
@@ -187,4 +187,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render asigna PORT, si no usa 5000
+    app.run(host='0.0.0.0', port=port, debug=False)  # host 0.0.0.0 para que sea accesible desde afuera
