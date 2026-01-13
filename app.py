5cec075bf093bce0be1129584c6585c85453d578
raizen1987 <53264750+raizen1987@users.noreply.github.com>
2026-01-13 09:30:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -681,7 +681,13 @@ def borrar_usuario(id):
     db.session.commit()
     flash('Usuario borrado', 'success')
     return redirect(url_for('usuarios'))
-
+    
+@app.route('/initdb')
+def initdb():
+    with app.app_context():
+        db.create_all()
+    return "Tablas creadas!"
+    
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5000))
    app.run(host="0.0.0.0", port=port, debug=False)  # debug=False en producción! 
